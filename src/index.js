e1358278cc7f098b76c43981d8673e05c9f1bb7d
Austin Howard <ahoward2@unca.edu>
2021-09-17 17:06:08 -0400

    optional application languages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const inquirer = require("inquirer");
 const shell = require("shelljs");
+
 inquirer
   .prompt([
     {
@@ -9,9 +10,33 @@ inquirer
       name: "app_name",
       default: "my-rs-app",
     },
+    {
+      type: "list",
+      message: "Pick a language:",
+      name: "app_language",
+      choices: ["typescript", "strict-typescript"],
+      default: "typescript",
+    },
   ])
   .then((answer) => {
+    let branch = "";
+
+    switch (answer.app_language) {
+      case "strict-typescript":
+        branch = "strict-typescript";
+        break;
+      default:
+        branch = "main";
+    }
+
     shell.exec(
-      `git clone https://github.com/SketchLagoon/rocket-science.git ${answer.app_name}`
+      `git clone -b ${branch} https://github.com/SketchLagoon/rocket-science.git ${answer.app_name}`
+    );
+    shell.echo(`Successfully Created A New Rocket Science App 🚀🧪`);
+    shell.echo(`Get started by running the following commands 👇`);
+    shell.echo(`1. yarn`);
+    shell.echo(`2. yarn test`);
+    shell.echo(
+      `3. yarn start (start the application server) or yarn storybook (start the storybook server)`
     );
   });
