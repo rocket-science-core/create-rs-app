3635d344f8203026147975d64b09e17080c1f412
Austin Howard <ahoward2@unca.edu>
2021-09-16 23:28:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# create-rs-app
+
+> A node cli for creating a new rocket-science application.
+
+## Run Locally
+
+```
+git clone https://github.com/ahoward2/create-rs-app.git
+yarn
+yarn start
+```
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const inquirer = require("inquirer");
+const shell = require("shelljs");
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Pick the name of your rocket-science app:",
+      name: "app_name",
+      default: "my-rs-app",
+    },
+  ])
+  .then((answer) => {
+    shell.exec(
+      `git clone https://github.com/SketchLagoon/rocket-science.git ${answer.app_name}`
+    );
+  });
