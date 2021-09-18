1bf8f58bc72de8afd8388f6b98f21e41f2a1d009
Austin Howard <ahoward2@unca.edu>
2021-09-17 22:19:32 -0400

    checkout with main branch name add instructions

--- src/index.js
+++ src/index.js
@@ -33,13 +33,35 @@ inquirer
     }
 
     shell.exec(
-      `git clone -b ${branch} https://github.com/SketchLagoon/rocket-science.git ${answer.app_name}`
+      `git clone --single-branch -b ${branch} https://github.com/SketchLagoon/rocket-science.git ${answer.app_name}`
     );
+    shell.cd(answer.app_name);
+    if (branch !== "main") {
+      shell.exec(`git checkout -b main`);
+      shell.exec(`git branch -d ${branch}`);
+    }
+    shell.echo(``);
     shell.echo(`Successfully Created A New Rocket Science App 🚀🧪`);
-    shell.echo(`Get started by running the following commands 👇`);
-    shell.echo(`1. yarn`);
-    shell.echo(`2. yarn test`);
+    shell.echo(``);
     shell.echo(
-      `3. yarn start (start the application server) or yarn storybook (start the storybook server)`
+      `---------- Get started by running the following commands 👇 ----------`
     );
+    shell.echo(``);
+    shell.echo(`1. cd ${answer.app_name}`);
+    shell.echo(`2. yarn`);
+    shell.echo(`3. yarn test`);
+    shell.echo(
+      `4. yarn start (start the application server) or yarn storybook (start the storybook server)`
+    );
+    shell.echo(``);
+    shell.echo(
+      `-------- Optional commands to push to your own git repository --------`
+    );
+    shell.echo(``);
+    shell.echo(
+      `5. git remote set-url origin https://github.com/user/repo.git (your remote repository)`
+    );
+    shell.echo(`6. git remote -v (verify new remote)`);
+    shell.echo(`7. git push -u origin main`);
+    shell.echo(``);
   });
